983422d62ebcf5ad9a1ef88ce0a4059f5de65eea
Tim Dolan <timmydolan14@gmail.com>
2023-04-26 11:05:47 -0400

    Updated Functionality for JS

--- index.js
+++ index.js
@@ -1,55 +1,59 @@
 // Establish Variables
 // const { default: inquirer } = require('inquirer');
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // Establish Questions
-inquirer.prompt ([
+inquirer.prompt([
     {
-
         type: 'input',
         name: 'title',
         message: 'What is the title of this project?',
-        
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'What is your Github Username?',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your Email Address?',
-        },
-        
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Please provide a description of this Project.',
-        },
-        
-        {
-            type: 'input',
-            name: 'instructions',
-            message: 'Please provide Instructions for this Project.',
-        },
-        
-        {
-            type: 'input',
-            name: 'contributions',
-            message: 'Can Users contribute to this project?',
-        },
-        
-        {
-            type: 'input',
-            name: 'tests',
-            message: 'How can Users test this project?',
-        },
-        
+
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github Username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your Email Address?',
+    },
+
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of this Project.',
+    },
+
+    {
+        type: 'input',
+        name: 'instructions',
+        message: 'Please provide Instructions for this Project.',
+    },
+
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'Can Users contribute to this project?',
+    },
+
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How can Users test this project?',
+    },
+
 ])
-.then((response) => {
-    console.log(response);
-});
+    .then((response) => {
+        console.log(response);
+        fs.writeFile('README_GEN.md', JSON.stringify(response, null, '\t'), (err) => {
+            err ? console.log(err) : console.log("Succcess");
+        });
+    })
+
 
 // Function to prompt questions above
 
